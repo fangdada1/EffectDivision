08bc98eb86f1fb0e71b27a3562766b9fc0d6246d
fangdada1 <fangbowen@fuya.live>
2023-05-25 14:00:06 +0800

    “update”

--- EffectDivision/Classes/Config.h
+++ EffectDivision/Classes/Config.h
@@ -8,7 +8,7 @@
 #ifndef Config_h
 #define Config_h
 
-#define LICENSE_NAME ((const char *)"fuya_20220816_20240815_com.funyas.vips_4.3.3.licbag")
+#define LICENSE_NAME ((const char *)"fuya_20230505_20240815_com.3pdoxu9v.uip_4.3.2_289.licbag")
 
 #define DEBUG_LOG false
 #define TIME_LOG false
